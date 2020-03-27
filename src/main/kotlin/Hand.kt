97b3a89b4e23052ca8bc0dc90498907a389c0899
Caleb <cmphillips412@gmail.com>
2020-03-27 14:48:23 -0400

    Hands are made up from a HashMap of all card values and get initialized by organizeCards. organizeCards checks for validity and throws an exception if illegal hand is created.

--- src/main/kotlin/Hand.kt
+++ src/main/kotlin/Hand.kt
@@ -1,13 +1,38 @@
-class Hand(cardOne: Card,
-           cardTwo: Card,
-           cardThree: Card,
-           cardFour: Card,
-           cardFive: Card) {
-    fun isValid(): Boolean  {
-        return true
+import java.lang.Exception
+
+data class Hand(
+    val cardOne: Card,
+    val cardTwo: Card,
+    val cardThree: Card,
+    val cardFour: Card,
+    val cardFive: Card) {
+
+    var isOrganized: Boolean = false
+
+    val cards: MutableMap<Value,MutableList<Card>> =
+        mutableMapOf(
+            Pair(Value.TWO,mutableListOf()),
+            Pair(Value.THREE, mutableListOf()),
+            Pair(Value.FOUR, mutableListOf()),
+            Pair(Value.FIVE, mutableListOf()),
+            Pair(Value.SIX, mutableListOf()),
+            Pair(Value.SEVEN, mutableListOf()),
+            Pair(Value.EIGHT, mutableListOf()),
+            Pair(Value.NINE, mutableListOf()),
+            Pair(Value.TEN, mutableListOf()),
+            Pair(Value.JACK, mutableListOf()),
+            Pair(Value.QUEEN, mutableListOf()),
+            Pair(Value.KING, mutableListOf()),
+            Pair(Value.ACE, mutableListOf()))
+
+    fun isValid(): Boolean =
+        setOf(cardOne,cardTwo,cardThree,cardFour,cardFive).distinctBy{Pair(it.value,it.suit)}.size == 5
+
+    fun organizeHand(){
+
+        val listOfCards = if(this.isValid()) listOf(cardOne,cardTwo,cardThree,cardFour,cardFive) else throw Exception("Cannot Organize invalid hand")
+
+        listOfCards.forEach { cards.get(it.value)!!.add(it)}
+        isOrganized = true
     }
-
-    val cards = listOf(cardOne, cardTwo, cardThree, cardFour, cardFive)
-
-
 }
--- src/test/kotlin/HandTest.kt
+++ src/test/kotlin/HandTest.kt
@@ -1,28 +1,49 @@
 
 import org.assertj.core.api.Assertions.assertThat
+import org.junit.Ignore
 import org.junit.Test
 
 class HandTest{
 
-    @Test
-    fun `A hand has exactly five cards`() {
-        val hand = Hand(Card(Value.FOUR, 'H'),
-                               Card(Value.FOUR, 'H'),
-                               Card(Value.FOUR, 'H'),
-                               Card(Value.FOUR, 'H'),
-                               Card(Value.FOUR, 'H'))
+    val invalidHand = Hand(
+        Card(Value.FOUR, 'H'),
+        Card(Value.FOUR, 'H'),
+        Card(Value.FOUR, 'H'),
+        Card(Value.FOUR, 'H'),
+        Card(Value.FOUR, 'H'))
 
-        assertThat(hand.cards.size).isEqualTo(5)
+    val validHand = Hand(
+        Card(Value.TWO, 'H'),
+        Card(Value.THREE, 'H'),
+        Card(Value.THREE, 'S'),
+        Card(Value.KING, 'H'),
+        Card(Value.JACK, 'H'))
+
+    @Test
+    fun `isValid returns true if it is`() {
+        assertThat(invalidHand.isValid()).isFalse()
+        assertThat(validHand.isValid()).isTrue()
     }
 
     @Test
-    fun `A Hand cannot have two cards of equal value and suit`() {
-        val invalidHand = Hand(Card(Value.FOUR, 'H'),
-            Card(Value.FOUR, 'H'),
-            Card(Value.FOUR, 'H'),
-            Card(Value.FOUR, 'H'),
-            Card(Value.FOUR, 'H'))
+    fun `organizeHand returns false if hand has not been organized`() {
+        assertThat(invalidHand.isOrganized).isFalse()
+        assertThat(validHand.isOrganized).isFalse()
+    }
 
-        assertThat(invalidHand.isValid()).isTrue()
+    @Test
+    fun `organizeHand throws an exception is hand isn't valid`() {
+        try{
+            invalidHand.organizeHand()
+        }catch(e: Exception){
+            assertThat(e.message).isEqualTo("Cannot Organize invalid hand")
+        }
+    }
+
+    @Test
+    fun `organizeHand return true if cards contains at least one list that isn't empty`() {
+        validHand.organizeHand()
+        assertThat(validHand.isOrganized).isTrue()
+        assertThat(validHand.cards.containsValue(listOf(Card(Value.KING,'H'))))
     }
 }

c8d6a4ff27b390736f74fa3588cd204d907bc158
Caleb <cmphillips412@gmail.com>
2020-03-31 17:09:18 -0400

    Refactored for Code Coverage

--- src/test/kotlin/HandTest.kt
+++ src/test/kotlin/HandTest.kt
@@ -1,29 +1,47 @@
-
 import org.assertj.core.api.Assertions.assertThat
 import org.junit.Test
 
-class HandTest{
+class HandTest {
 
     val validHand = Hand(
         Card(Value.TWO, 'H'),
         Card(Value.THREE, 'H'),
         Card(Value.THREE, 'S'),
         Card(Value.KING, 'H'),
-        Card(Value.JACK, 'H'))
+        Card(Value.JACK, 'H')
+    )
 
     val twoPairHand = Hand(
         Card(Value.TWO, 'H'),
         Card(Value.TWO, 'S'),
         Card(Value.THREE, 'S'),
         Card(Value.THREE, 'H'),
-        Card(Value.JACK, 'H'))
+        Card(Value.JACK, 'H')
+    )
 
     val threeOfAKindHand = Hand(
         Card(Value.TWO, 'H'),
         Card(Value.TWO, 'S'),
         Card(Value.TWO, 'C'),
         Card(Value.THREE, 'H'),
-        Card(Value.JACK, 'H'))
+        Card(Value.JACK, 'H')
+    )
+
+    @Test
+    fun `organizeHand throws an exception is hand isn't valid`() {
+
+        try {
+            Hand(
+                Card(Value.FOUR, 'H'),
+                Card(Value.FOUR, 'H'),
+                Card(Value.FOUR, 'H'),
+                Card(Value.FOUR, 'H'),
+                Card(Value.FOUR, 'H')
+            ).organizeHand()
+        } catch (e: Exception) {
+            assertThat(e.message).isEqualTo("Cannot Organize invalid hand")
+        }
+    }
 
     @Test
     fun `organizeHand returns false if hand has not been organized`() {
@@ -34,26 +52,26 @@ class HandTest{
     fun `organizeHand returns true if cards contains at least one list that isn't empty`() {
         validHand.organizeHand()
         assertThat(validHand.isOrganized).isTrue()
-        assertThat(validHand.cards.containsValue(listOf(Card(Value.KING,'H'))))
+        assertThat(validHand.cards.containsValue(listOf(Card(Value.KING, 'H'))))
     }
 
     @Test
     fun `determineHighCard finds the highest card in the hand`() {
-        val expectedCard = Card(Value.KING,'H')
+        val expectedCard = Card(Value.KING, 'H')
         assertThat(validHand.determineHighCard()).isEqualTo(expectedCard)
     }
 
     @Test
     fun `determineHighestPairedCard finds the highest paired card in the hand`() {
         twoPairHand.organizeHand()
-        val expectedCard = Card(Value.THREE,'S')
+        val expectedCard = Card(Value.THREE, 'S')
         assertThat(twoPairHand.determineHighestPairedCard()).isEqualTo(expectedCard)
     }
 
     @Test
     fun `determineThreeOfAKindCard finds a card whose value matches the three of a kind in the hand`() {
         threeOfAKindHand.organizeHand()
-        val expectedCard = Card(Value.TWO,'H')
+        val expectedCard = Card(Value.TWO, 'H')
         assertThat(threeOfAKindHand.determineThreeOfAKindCard().value).isEqualTo(expectedCard.value)
     }
 }
